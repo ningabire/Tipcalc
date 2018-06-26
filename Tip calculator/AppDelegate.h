2f10052affa4b5a684570095c3b09ff499d22a91
Norette Ingabire <noretteingabire@fb.com>
2018-06-26 13:32:04 -0700

    Initial Commit

---
+++ Tip calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tip calculator
+//
+//  Created by Norette Ingabire on 6/26/18.
+//  Copyright © 2018 Norette Ingabire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tip calculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tip calculator
+//
+//  Created by Norette Ingabire on 6/26/18.
+//  Copyright © 2018 Norette Ingabire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
